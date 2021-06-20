d516c71e23fd17d53470fdd1641ba250ce546058
Hakan <35165032+HakanOzr@users.noreply.github.com>
2021-06-20 22:54:39 +0300

    Add files via upload

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+func main() {
+
+	//----------------------  Write to File
+
+	// cards := newDeck()
+	// cards.saveToFile("Kartlarım")
+
+	//-----------------  Read from File.
+
+	//cards := readFromFile("Kartlarım")
+	//cards.iterateAndPrint()
+
+	//---------------  Create a new deck and deal the deck.
+
+	//cards := newDeck()
+	// 	hand, remainingCards := dealDeck(cards, 5)
+	// 	hand.iterateAndPrint()
+	// 	remainingCards.iterateAndPrint()
+
+	// ------------  Shuffle the cards.
+
+	// cards := newDeck()
+	// cards.shuffle()
+	// cards.iterateAndPrint()
+
+}
